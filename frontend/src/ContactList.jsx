8d54ce05d73963dc8967736d4242800bc7f752d3
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-02-20 21:34:11 +0800

    initial commit

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,39 @@
+//import React from "react";
+import PropTypes from "prop-types";
+
+const ContactList = ({ contacts }) => {
+  return (
+    <div>
+      <h3>Contacts</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>E-mail</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.firstName}</td>
+              <td>{contact.lastName}</td>
+              <td>{contact.email}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.string,
+};
+
+export default ContactList;
